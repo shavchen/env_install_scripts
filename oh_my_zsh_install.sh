f85c4daf156245052d4d8e37e0544960d3a9bd04
shavchen <34539839+shavchen@users.noreply.github.com>
2021-07-17 23:48:27 +0800

    Update oh_my_zsh_install.sh

--- oh_my_zsh_install.sh
+++ oh_my_zsh_install.sh
@@ -13,25 +13,30 @@ if [ -f "/usr/bin/zsh" ]; then
   if [ ! -d  ~/.oh-my-zsh ]; then
     ZSH= sh -c "$(curl -fsSL https://raw.githubusercontent.com/shavchen/env_install_scripts/main/oh-my-zsh.sh)" > /dev/null 2>&1
     echo -e "\e[0;32m 已安装 oh-my-zsh \e[0m"
+  else
+    echo -e "\e[0;31m oh-my-zsh exists...."
   fi
-  if [ ! -d "~/.oh-my-zsh/plugins/zsh-autosuggestions" ]; then
-    git clone git://github.com/zsh-users/zsh-autosuggestions  ~/.oh-my-zsh/plugins/zsh-autosuggestions > /dev/null 2>&1
-    source ~/.oh-my-zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+  if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+    git clone git://github.com/zsh-users/zsh-autosuggestions  ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions > /dev/null 2>&1
+    cd ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+    source zsh-autosuggestions.zsh
     echo -e "\e[0;32m 已下载zsh-autosuggestions\e[0m"
   else
-    echo "[*] ~/.oh-my-zsh/plugins/zsh-autosuggestions exists..."
+    echo -e "\e[0;31m zsh-autosuggestions exists..."
   fi
-  if [ ! -d "~/.oh-my-zsh/plugins/zsh-syntax-highlighting" ]; then
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting > /dev/null 2>&1
-    source ~/.oh-my-zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+  if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-highlighting ]; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting > /dev/null 2>&1
     echo -e "\e[0;32m 已下载zsh-syntax-highlighting.\e[0m"
+    cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting/
+    source zsh-syntax-highlighting.zsh
   else
-    echo "[*]  ~/.oh-my-zsh/plugins/zsh-syntax-highlighting  exists...."
+    echo -e "\e[0;31m zsh-syntax-highlighting  exists...."
   fi
  # 设置.zshrc
-  cp ~/.zshrc ~/.zshrc.bak
-  wget https://raw.githubusercontent.com/shavchen/env_install_scripts/main/.zshrc -O ~/.zshrc > /dev/null 2>&1
-  echo -e "\e[0;32m 请手动执行 source ~/.zshrc 使配置生效\e[0m"
+  cp ~/.zshrc ~/.zshrc.bak 
+  source ~/.zshrc
+  echo -e "\e[0;32m 配置生效 \e[0m"
 else
   echo -e "\e[0;31m 请安装zsh\e[0m"
 fi
+cd
